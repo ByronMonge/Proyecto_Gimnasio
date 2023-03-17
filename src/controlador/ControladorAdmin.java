887401d8e064df2dd897fdb42563a7e43d95fbdf
Michu0908 <127367474+Michu0908@users.noreply.github.com>
2023-03-16 22:56:20 -0500

    no hice nada

---
+++ src/controlador/ControladorAdmin.java
@@ -0,0 +1,20 @@
+package controlador;
+
+import java.util.Date;
+import java.util.List;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import modelo.Administrador;
+import modelo.ModeloAdministrador;
+import modelo.ModeloPersona;
+import vista.VistaAdmin;
+import vista.VistaPrincipal;
+
+/**
+ *
+ * @author miche
+ */
+public class ControladorAdmin {
+
+    
+}
