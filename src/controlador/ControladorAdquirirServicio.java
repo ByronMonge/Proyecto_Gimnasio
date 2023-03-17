24c80f1ea1bcced66345b0ff372603cc9f9c300f
ByronMonge <byron.monge.est@tecazuay.edu.ec>
2023-03-17 04:42:12 -0500

    No tocar. Casi terminado

--- src/controlador/ControladorAdquirirServicio.java
+++ src/controlador/ControladorAdquirirServicio.java
@@ -35,10 +35,17 @@ public class ControladorAdquirirServicio {
     }
 
     public void iniciarControl() {
+
+        cargarTablaDeAdquirirServicio();//Se cargan los datos apenas se inicia la ventana
+
         //AdquirirServicio
         vista.getBtnCrear().addActionListener(l -> abrirjDialogAdquirirServicio());
         vista.getBtnCalcular().addActionListener(l -> calcularTotal());
-        vista.getBtnGuardar().addActionListener(l-> crearModificarAdquirirServicio());
+        vista.getBtnGuardar().addActionListener(l -> crearModificarAdquirirServicio());
+        vista.getBtnEliminar().addActionListener(l -> eliminarAdquirirServicio());
+        vista.getBtnActualizar().addActionListener(l -> cargarTablaDeAdquirirServicio());
+        vista.getBtnModificar().addActionListener(l -> cargarDatosAdquirirServicioEnTXT());
+        buscarRegistros();
 
         //Cliente
         vista.getBtnBuscarCli().addActionListener(l -> abrirjDialogCliente());
@@ -87,6 +94,7 @@ public class ControladorAdquirirServicio {
         List<Cliente> clientes = cliente.listaClientesTabla();
         List<Instructor> instructores = instructor.listaInstructoresTabla();
         List<Nutricionista> nutricionistas = nutricionista.listaNutricionistaTabla();
+        List<Servicio> servicios = servicio.listaServiciosTabla();
 
         adquirir.stream().forEach(a -> {
 
@@ -97,8 +105,20 @@ public class ControladorAdquirirServicio {
 
                         if (a.getAdq_codins() == i.getIns_codigo()) {
 
-                            Object[] datos = {a.getAdq_codigo(), c.getPer_cedula(), c.getPer_nombre() + " " + c.getPer_apellido(), i.getPer_nombre() + " " + i.getPer_apellido()};
-                            tabla.addRow(datos);
+                            nutricionistas.stream().forEach(n -> {
+
+                                if (a.getAdq_codnut() == n.getNutri_codigo()) {
+
+                                    servicios.stream().forEach(s -> {
+
+                                        if (a.getAdq_codser() == s.getSer_codigo()) {
+
+                                            Object[] datos = {a.getAdq_codigo(), c.getPer_cedula(), c.getPer_nombre() + " " + c.getPer_apellido(), i.getPer_nombre() + " " + i.getPer_apellido(), n.getPer_nombre() + " " + n.getPer_apellido(), s.getSer_nombre(), a.getAdq_costototal()};
+                                            tabla.addRow(datos);
+                                        }
+                                    });
+                                }
+                            });
                         }
                     });
                 }
@@ -124,6 +144,7 @@ public class ControladorAdquirirServicio {
             if (encontrar) {
                 JOptionPane.showMessageDialog(null, "Solo puede adquirir un servicio");
             } else {
+                //Validar
                 modelo.setAdq_codcli(Integer.parseInt(vista.getTxtCodigoCliente().getText()));
                 modelo.setAdq_codins(Integer.parseInt(vista.getTxtCodigoInstructor().getText()));
                 modelo.setAdq_codnut(Integer.parseInt(vista.getTxtCodigoNutricionista().getText()));
@@ -145,7 +166,179 @@ public class ControladorAdquirirServicio {
                     JOptionPane.showMessageDialog(null, "No se pudo adquirir el servicio");
                 }
             }
+        } else {
+            //Validar
+            modelo.setAdq_codigo(Integer.parseInt(vista.getTxtCodigoAdquirirServicio().getText()));
+            modelo.setAdq_codcli(Integer.parseInt(vista.getTxtCodigoCliente().getText()));
+            modelo.setAdq_codins(Integer.parseInt(vista.getTxtCodigoInstructor().getText()));
+            modelo.setAdq_codnut(Integer.parseInt(vista.getTxtCodigoNutricionista().getText()));
+            modelo.setAdq_codser(Integer.parseInt(vista.getTxtCodigoServicio().getText()));
+
+            modelo.setAdq_mesesins(Integer.parseInt(vista.getSpinnerMesesIns().getValue().toString()));
+            modelo.setAdq_mesesnut(Integer.parseInt(vista.getSpinnerMesesNut().getValue().toString()));
+            modelo.setAdq_mesesser(Integer.parseInt(vista.getSpinnerMesesSer().getValue().toString()));
+
+            Date fecha = vista.getFechaInicio().getDate();
+            java.sql.Date fechaSQL = new java.sql.Date(fecha.getTime());
+            modelo.setAdq_fechainicio(fechaSQL);
+
+            modelo.setAdq_costototal(Double.parseDouble(vista.getTxtTotal().getText()));
+
+            if (modelo.modificarAdquirirServicio()) {
+                JOptionPane.showMessageDialog(null, "El servicio se modifico satisfactoriamente");
+            } else {
+                JOptionPane.showMessageDialog(null, "No se pudo modificar el servicio");
+            }
         }
+
+        cargarTablaDeAdquirirServicio();
+    }
+
+    public void cargarDatosAdquirirServicioEnTXT() {
+        int fila = vista.getTblAdquirirServicio().getSelectedRow();
+
+        if (fila == -1) {
+            JOptionPane.showMessageDialog(null, "Aun no ha seleccionado una fila");
+        } else {
+
+            vista.getjDlgAdquirirServicio().setVisible(true);
+            vista.getjDlgAdquirirServicio().setName("Modificar instructor");
+            vista.getjDlgAdquirirServicio().setSize(883, 546);
+            vista.getjDlgAdquirirServicio().setLocationRelativeTo(null);
+            vista.getjDlgAdquirirServicio().setTitle("Modificar instructor");
+            //bloquearCampos();
+
+            //Quitar visibilidad
+            //vista.getTxtCodigoAdquirirServicio().setVisible(false);
+            ModeloCliente cliente = new ModeloCliente();
+            ModeloInstructor instructor = new ModeloInstructor();
+            ModeloNutricionista nutricionista = new ModeloNutricionista();
+            ModeloServicio servicio = new ModeloServicio();
+
+            List<AdquirirServicio> adquirir = modelo.listaAdquirirServicioTabla();
+            List<Cliente> clientes = cliente.listaClientesTabla();
+            List<Instructor> instructores = instructor.listaInstructoresTabla();
+            List<Nutricionista> nutricionistas = nutricionista.listaNutricionistaTabla();
+            List<Servicio> servicios = servicio.listaServiciosTabla();
+
+            adquirir.stream().forEach(a -> {
+
+                clientes.stream().forEach(c -> {
+                    if (a.getAdq_codcli() == c.getCliente_codigo()) {
+
+                        instructores.stream().forEach(i -> {
+
+                            if (a.getAdq_codins() == i.getIns_codigo()) {
+
+                                nutricionistas.stream().forEach(n -> {
+
+                                    if (a.getAdq_codnut() == n.getNutri_codigo()) {
+
+                                        servicios.stream().forEach(s -> {
+
+                                            if (a.getAdq_codser() == s.getSer_codigo()) {
+
+                                                //Adquirir servicio
+                                                vista.getTxtCodigoAdquirirServicio().setText(String.valueOf(a.getAdq_codigo()));
+                                                vista.getFechaInicio().setDate(a.getAdq_fechainicio());
+                                                vista.getTxtTotal().setText(String.valueOf(a.getAdq_costototal()));
+
+                                                //Cliente
+                                                vista.getTxtCodigoCliente().setText(String.valueOf(c.getCliente_codigo()));
+                                                vista.getTxtCedulaCli().setText(c.getPer_cedula());
+                                                vista.getTxtNombreCli().setText(c.getPer_nombre() + " " + c.getPer_apellido());
+
+                                                //Instructor
+                                                vista.getTxtCodigoInstructor().setText(String.valueOf(i.getIns_codigo()));
+                                                vista.getTxtCedulaIns().setText(i.getPer_cedula());
+                                                vista.getTxtNombreIns().setText(i.getPer_nombre() + " " + i.getPer_apellido());
+                                                vista.getSpinnerSueldoIns().setValue(i.getIns_suedo());
+                                                vista.getSpinnerMesesIns().setValue(a.getAdq_mesesins());
+
+                                                //Nutricionista
+                                                vista.getTxtCodigoNutricionista().setText(String.valueOf(n.getNutri_codigo()));
+                                                vista.getTxtCedulaNut().setText(n.getPer_cedula());
+                                                vista.getTxtNombreNut().setText(n.getPer_nombre() + " " + n.getPer_apellido());
+                                                vista.getSpinnerSueldoNut().setValue(n.getNutri_salario());
+                                                vista.getSpinnerMesesNut().setValue(a.getAdq_mesesnut());
+
+                                                //Servicio
+                                                vista.getTxtCodigoServicio().setText(String.valueOf(s.getSer_codigo()));
+                                                vista.getTxtNombreSer().setText(s.getSer_nombre());
+                                                vista.getSpinnerCostoSer().setValue(s.getSer_precio());
+                                                vista.getSpinnerMesesSer().setValue(a.getAdq_mesesser());
+                                            }
+                                        });
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+            });
+        }
+    }
+
+    public void eliminarAdquirirServicio() {
+
+        int fila = vista.getTblAdquirirServicio().getSelectedRow();
+
+        if (fila == -1) {
+            JOptionPane.showMessageDialog(null, "Aun no ha seleccionado una fila");
+        } else {
+
+            int response = JOptionPane.showConfirmDialog(vista, "¿Seguro que desea eliminar esta información?", "Confirmar", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+            if (response == JOptionPane.YES_OPTION) {
+
+                int codigoAdquirirServicio;
+                codigoAdquirirServicio = Integer.parseInt(vista.getTblAdquirirServicio().getValueAt(fila, 0).toString());
+
+                if (modelo.eliminarAdquirirServicio(codigoAdquirirServicio)) {
+                    JOptionPane.showMessageDialog(null, "El registro se elimino satisfactoriamente");
+                    cargarTablaDeAdquirirServicio();//Actualizo la tabla con los datos
+                } else {
+                    JOptionPane.showMessageDialog(null, "El registro no se pudo eliminar");
+                }
+            }
+        }
+    }
+
+    public void buscarRegistros() {
+
+        KeyListener eventoTeclado = new KeyListener() {//Crear un objeto de tipo keyListener(Es una interface) por lo tanto se debe implementar sus metodos abstractos
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+                //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+                //CODIGO PARA FILTRAR LOS DATOS DIRECTAMENTE DE LA TABLA. NO ELIMINAR. SI FUNCIONA. ES MUY IMPORTANTE
+                TableRowSorter<DefaultTableModel> filtrar;
+
+                DefaultTableModel tabla = (DefaultTableModel) vista.getTblAdquirirServicio().getModel();
+
+                //vista.getTablaconduccion().setAutoCreateRowSorter(true);
+                filtrar = new TableRowSorter<>(tabla);
+                vista.getTblAdquirirServicio().setRowSorter(filtrar);
+
+                try {
+
+                    filtrar.setRowFilter(RowFilter.regexFilter(vista.getTxtBuscar().getText())); //Se pasa como parametro el campo de donde se va a obtener la informacion y el (3) es la columna con la cual va a buscar las coincidencias
+                } catch (Exception ex) {
+                    System.out.println("Error: " + ex);
+                }
+            }
+        };
+
+        vista.getTxtBuscar().addKeyListener(eventoTeclado); //"addKeyListener" es un metodo que se le tiene que pasar como argumento un objeto de tipo keyListener 
     }
 
     //Todo sobre el registro de clientes en el jDialog
